7ae694e21754a91feeb1fa581475ec2d3f9d6c84
Shmuel Maruani <13782917+101sam@users.noreply.github.com>
2020-12-08 08:42:23 -0800

    Update adding-scripts-path.sh

--- adding-scripts-path.sh
+++ adding-scripts-path.sh
@@ -2,7 +2,7 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "# The next line updates PATH for the WP248 scripts." >> /etc/profile
+echo "# The next line updates PATH for the limitlessv scripts." >> /etc/profile
 echo "# added by limitlessv script" >> /etc/profile
 echo "if [ -f '/opt/limitlessv/scripts/path.bash.inc' ]; then . '/opt/limitlessv/scripts/path.bash.inc'; fi" >> /etc/profile
 
